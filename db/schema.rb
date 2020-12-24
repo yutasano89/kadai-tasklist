63a78b22dba0f01d2e0edbf0064b31a92c0bc613
yuta.sano <yutasano89@gmail.com>
2020-12-24 10:58:01 +0000

    status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_112645) do
+ActiveRecord::Schema.define(version: 2020_12_23_123622) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
