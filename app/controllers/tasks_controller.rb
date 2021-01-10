d3ea4d5836c003c01fa1078c2ca941a873db2751
yuta.sano <yutasano89@gmail.com>
2021-01-10 13:09:20 +0000

    login rev1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
   end
     
   def task_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
-end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
