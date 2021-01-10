f052377a9a8080a79d01e61423d017ec1579389a
yuta.sano <yutasano89@gmail.com>
2021-01-10 13:37:22 +0000

    login rev2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc)
